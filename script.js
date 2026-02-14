6df3a9d496878910c19557362a54fc49b2847b30
jirenvathan-hue <jirenvathan@gmail.com>
2026-02-14 22:54:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const song = document.getElementById("song");
 
+const playlist = [
+    "music/maroonsugar-song.mp3",
+    "music/perfect-song.mp3",
+    "music/suttumvizhi-song.mp3"
+];
+
+let songIndex = 0;
+
+// Move NO button 😈
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * window.innerWidth * 0.8;
     const y = Math.random() * window.innerHeight * 0.8;
@@ -9,8 +18,22 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.top = y + "px";
 });
 
-yesBtn.addEventListener("click", () => {
+// Play playlist 🎵
+function playNextSong() {
+    song.src = playlist[songIndex];
     song.play();
+}
+
+song.addEventListener("ended", () => {
+    songIndex++;
+    if (songIndex < playlist.length) {
+        playNextSong();
+    }
+});
+
+// YES click 💖
+yesBtn.addEventListener("click", () => {
+    playNextSong();
 
     confetti({
         particleCount:200,
@@ -21,3 +44,4 @@ yesBtn.addEventListener("click", () => {
     document.querySelector(".container").style.display="none";
     document.getElementById("lovePage").classList.remove("hidden");
 });
+
